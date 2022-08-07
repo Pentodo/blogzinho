b5909d71644760ec0543f7ac7c235352d99aaa70
Matheus Alberto <matheus.a.v.b@gmail.com>
2022-08-07 05:40:29 -0300

    login page

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,7 +1,32 @@
+import styles from './Login.module.css';
+
 const Login = () => {
 	return (
-		<div>
-			<h1>login</h1>
+		<div className={styles.Login}>
+			<h1>Login</h1>
+			<p>Crie ou logue em sua conta abaixo:</p>
+			<div className={styles.forms}>
+				<form>
+					<h2>Registrar</h2>
+					<label>
+						E-mail: <input type='email' id='register-email' />
+					</label>
+					<label>
+						Senha: <input type='password' id='register-password' />
+					</label>
+					<input type='button' value='>>' />
+				</form>
+				<form>
+					<h2>Entrar</h2>
+					<label>
+						E-mail: <input type='email' id='login-email' />
+					</label>
+					<label>
+						Senha: <input type='password' id='login-password' />
+					</label>
+					<input type='button' value='>>' />
+				</form>
+			</div>
 		</div>
 	);
 };
